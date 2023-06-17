8102fc7e4f459955eb1b44ab36fc925aa0a7005e
Oladitan Saliu <sablik4all@gmail.com>
2023-06-17 12:34:48 -0700

    create home and profile modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './modules/home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule} from "@angular/material/card";
 import { MatButtonModule} from "@angular/material/button";
@@ -10,11 +11,14 @@ import { SharedModule } from './modules/shared/shared.module';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
+import { ProfileComponent } from './modules/profile/profile.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,    
+    HomeComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/modules/home/home.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Observable, Subscription, of } from 'rxjs';
+
+@Component({
+    selector: 'app-home',
+    templateUrl: 'home.component.html',
+})
+
+export class HomeComponent {
+    title = 'my-course-app';
+    headerTitle: string | undefined = "This is a header";
+    isHeaderVisible: boolean = true;
+  
+    name: string | undefined;
+    surname: string | undefined;
+  
+    formGroup: FormGroup | undefined;
+  
+    constructor(
+      private formBuilder: FormBuilder
+    ) {
+    }
+    
+    observable$: Observable<number> = of(1,2,3,4,5);
+  
+    numberSubscription: Subscription | undefined;
+    formSubscription: Subscription | undefined;
+    
+    ngOnInit(): void {
+      this.displayObservableNumbers();
+      this.formGroup = this.formBuilder.group({
+        name: [undefined, [Validators.required, Validators.email, 
+        Validators.minLength(5)]],
+        surname: [undefined],
+      });
+      this.observeFormChanges();
+    }
+  
+    private displayObservableNumbers(): void {
+      this.numberSubscription = this.observable$.subscribe((value) => {
+        console.log('Here I am');      
+        console.log(value);      
+      })
+    }
+  
+    private observeFormChanges(): void {
+      this.formSubscription = this.formGroup?.valueChanges.subscribe((val) =>{
+        console.log(val);
+        
+      })
+    }
+  
+    onFormSubmit(): void{
+      console.log(this.formGroup?.valid);    
+      console.log(this.formGroup?.value);
+    }
+  
+    ngOnDestroy(): void {
+      this.numberSubscription?.unsubscribe();
+      this.formSubscription?.unsubscribe();
+    }
+}
